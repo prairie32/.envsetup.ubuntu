4f3fdbe07e7cc8be16d9806bb835c46b9db22b45
Kay Lee <k.nousco@gmail.com>
2013-12-11 23:48:03 -0800

    ..

--- devsetup.sh
+++ devsetup.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-#export ADBHOST=172.30.117.185
-#export ADBHOSTPORT=$ADBHOST:5555
-
-#export HOME_MOUNTED=/usr/local/google/home/kihwanl
-#export WORK_MOUNTED=/media/work2
 export WORK_MOUNTED=~/work
-#export WORK_MOUNTED=$HOME_MOUNTED/work2
 
 case $OSTYPE in
 	darwin*) OS=macosx;;
@@ -16,23 +10,6 @@ esac
 ENVSETUP=~/.envsetup.$OS
 
 export TOOL_MOUNTED=$WORK_MOUNTED/tool
-export SOURCEDIR=$WORK_MOUNTED/source/android
-
-export AOSPDIR=$SOURCEDIR/aosp
-export ANDROIDDIR=$SOURCEDIR/android
-export GOOGLETVDIR=$SOURCEDIR/googletv
-export CTSDIR=$SOURCEDIR/android-cts/android-cts-3.2_r5
-export GCTSDIR=$SOURCEDIR/android-gcts/android-cts
-export CTSTOOLSDIR=$CTSDIR/tools
-export GCTSTOOLSDIR=$GCTSDIR/tools
-export CTSREPODIR=$CTSDIR/repository
-export GCTSREPODIR=$GCTSDIR/repository
-export CTSTESTCASESDIR=$CTSREPODIR/testcases
-export GCTSTESTCASESDIR=$GCTSREPODIR/testcases
-export DEVADMIN_APK_FOR_CTS2dot3R4=$CTSTESTCASESDIR/CtsDeviceAdmin.apk
-export GDEVADMIN_APK_FOR_CTS2dot3R4=$GCTSTESTCASESDIR/CtsDeviceAdmin.apk
-export DELEGATEACCESSSERVICE_APK_FOR_CTS2dot1R2=$CTSTESTCASESDIR/CtsDelegatingAccessibilityService.apk
-export GDELEGATEACCESSSERVICE_APK_FOR_CTS2dot1R2=$GCTSTESTCASESDIR/CtsDelegatingAccessibilityService.apk
 
 export COLOR_PY=$ENVSETUP/coloredlogcat.py
 export ENVSETUP_SH=$ENVSETUP/envsetup.sh
@@ -58,63 +35,15 @@ export DO_ALL_AT_ONCE_SH=$ENVSETUP/do_all_at_once.sh
 
 export JAVA_HOME=~/tool/jdk1.6.0_45
 export ANDROID_JAVA_HOME=$JAVA_HOME
-#export WORKSPACE=$TOOL_MOUNTED/eclipse/workspace
-export ADT_BUNDLE=$TOOL_MOUNTED/adt-bundle-linux
-export WORKSPACE=$ADT_BUNDLE/workspace
-export ANDROID_SWT=$GITCORPDIR/prebuilt/linux-x86_64/swt
 export USE_CCACHE=1
 export CCACHE_DIR=~/tool/ccache
-#export GDB=$GOOGLETVDIR/gitcorp_2/prebuilt/linux-x86_64/toolchain/arm-unknown-linux-gnueabi-4.5.3-glibc/bin/arm-unknown-linux-gnueabi-gdb
-export GDB=$GOOGLETVDIR/gitv4.0/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-gdb
 
 # set the number of open files to be 1024 for android
 ulimit -S -n 1024
 
-export adt_bundle_ubuntu=$TOOL_MOUNTED/android-adt/adt-bundle-linux-x86_64-20130219
-export sdk=$adt_bundle_ubuntu/sdk
-export skindir=$sdk/platforms/android-16/skins
-sdcard=~/.android/sdcard.img
-keyset=~/.android/default.keyset
-emulator_cmd_common="emulator -sysdir $OUT -data $OUT/userdata.img -sdcard $sdcard -memory 1024 -gpu on -camera-front webcam0"
-alias ls.skins='l $skindir'
-alias emulator.build='$emulator_cmd_common'
-alias emulator.build.skin='$emulator_cmd_common -skindir $skindir -skin '
-
-alias agdb='$GDB'
-
-alias vijdbrc='vi $ENVSETUP/.jdbrc'
-alias ln.gdbinit='ln -s $ENVSETUP/.gdbinit .gdbinit'
-alias ln.jdbrc='ln -s $ENVSETUP/.jdbrc .jdbrc'
-gdb_name=ln.gdb
-alias ln.gdb='rm -f $gdb_name; ln -s $ANDROID_TOOLCHAIN/*gdb $gdb_name'
-symbols_name=ln.symbols
-alias ln.symbols='rm -f $symbols_name; ln -s $OUT/symbols $symbols_name'
-
-alias cdroot='cd ../../../../..; ll'
-alias cdres='cd ../../../../../res/; ll'
-alias cdlayout='cd ../../../../../res/layout; ll'
-alias cdwork='cd $WORK_MOUNTED'
-alias cdgoogletv='cd $GOOGLETVDIR'
-alias cdworkspace='cd $WORKSPACE'
-alias cdcts='cd $CTSTOOLSDIR'
-alias cdgcts='cd $GCTSTOOLSDIR'
-
 alias whereaminow='$WHEREAMINOW_SH'
 alias whatamidoingnow='$WHATAMIDOINGNOW_SH'
 alias gotowiththesamedepth='$GOTOWITHTHESAMEDEPTH_SH'
-
-alias gotogit='. $GOTOWITHTHESAMEDEPTH_SH gitcorp'
-alias gotogit0='. $GOTOWITHTHESAMEDEPTH_SH gitcorp_0'
-alias gotogit1='. $GOTOWITHTHESAMEDEPTH_SH gitcorp_1'
-alias gotogit2='. $GOTOWITHTHESAMEDEPTH_SH gitcorp_2'
-alias gotogit3='. $GOTOWITHTHESAMEDEPTH_SH gitcorp_3'
-alias gotogitdbg='. $GOTOWITHTHESAMEDEPTH_SH gitcorp_dgb'
-alias gotogitv2.1='. $GOTOWITHTHESAMEDEPTH_SH gitv2.1'
-alias gotogitv3.0='. $GOTOWITHTHESAMEDEPTH_SH gitv3.0'
-alias gotogitv4.0='. $GOTOWITHTHESAMEDEPTH_SH gitv4.0'
-alias gotolg='. $GOTOWITHTHESAMEDEPTH_SH partnerlg'
-alias gotolg0='. $GOTOWITHTHESAMEDEPTH_SH partnerlg_0'
-alias gotolgv3.0='. $GOTOWITHTHESAMEDEPTH_SH lgv3.0'
 
 alias activitiesOnce='$ADBCON_SH && adb -s $ADBHOSTPORT shell dumpsys activity activities | grep Run'
 alias activities='$ADBCON_SH && $REPEAT_SH 1 adb -s $ADBHOSTPORT shell dumpsys activity activities | grep Run'
@@ -176,79 +105,6 @@ alias activities='$ADBCON_SH && $REPEAT_SH 1 adb -s $ADBHOSTPORT shell dumpsys a
 alias killprocess='$KILLPROCESS_SH'
 alias killqemu='$KILLPROCESS_SH qemu'
 
-alias createapp.help='echo "Example: android create project -n ProjectName -t 1 -p ./projectname -k com.example.projectname -a ProjectActivity"; android --help create project'
-alias createapp='android create project'
-alias updateapp.help='echo "Example: android update project -l $TOOL_MOUNTED/android-sdk-linux/platforms/android-11 -p ./projectname"; android --help update project'
-alias updateapp='android update project'
-
-#alias eclipse='eclipse -vm $TOOL_MOUNTED/jdk1.6.0_33/bin &'
-
-#alias lunch.android.master='cd $ANDROIDDIR/master; . build/envsetup.sh; lunch full-eng; source.devsetup'
-#alias lunch.android.jb-mr1.1-dev='cd $ANDROIDDIR/jb-mr1.1-dev; . build/envsetup.sh; lunch full-eng; source.devsetup'
-#alias lunch.android.jb-mr1-aah-dev='cd $ANDROIDDIR/jb-mr1-aah-dev; . build/envsetup.sh; lunch full-eng; source.devsetup'
-
-alias lunch.aosp.master='cd $AOSPDIR/master; . build/envsetup.sh; lunch full-eng; source.devsetup'
-#alias lunch.aosp.jb-mr1-dev='cd $AOSPDIR/jb-mr1-dev; . build/envsetup.sh; lunch full-eng; source.devsetup'
-#alias lunch.aosp.android-4.2.1_r1='cd $AOSPDIR/android-4.2.1_r1; . build/envsetup.sh; lunch full-eng; source.devsetup'
-alias lunch.aosp.android-4.2.2_r1='cd $AOSPDIR/android-4.2.2_r1; . build/envsetup.sh; lunch full-eng; source.devsetup'
-
-#alias lunch.gtv.3.0.cosmo='cd $GOOGLETVDIR/gitcorp_2; . build/envsetup.sh; lunch cosmo-eng; source.envsetup'
-
-#alias lunch.gtv.4.0-mr0='cd $GOOGLETVDIR/gtv-4.0-jb-mr0; . build/envsetup.sh; lunch cosmo-eng; source.devsetup'
-#alias build.gtv.4.0-mr0='cd $GOOGLETVDIR/gtv-4.0-jb-mr0; . build/envsetup.sh; lunch cosmo-eng; source.devsetup; makewithlog; makeotawithlog; gtv_reinstall'
-#alias night.gtv.4.0-mr0='cd $GOOGLETVDIR/gtv-4.0-jb-mr0; . build/envsetup.sh; lunch cosmo-eng; source.devsetup; repo sync -j20; makewithlog; makeotawithlog'
-#alias lunch.gtv.4.0-mr0.berlin='cd $GOOGLETVDIR/gtv-4.0-jb-mr0.berlin; . build/envsetup.sh; lunch berlin-eng; source.devsetup'
-#alias build.gtv.4.0-mr0.berlin='cd $GOOGLETVDIR/gtv-4.0-jb-mr0.berlin; . build/envsetup.sh; lunch berlin-eng; source.devsetup; makewithlog; makeotawithlog; gtv_reinstall'
-#alias night.gtv.4.0-mr0.berlin='cd $GOOGLETVDIR/gtv-4.0-jb-mr0.berlin; . build/envsetup.sh; lunch berlin-eng; source.devsetup; repo sync -j20; makewithlog; makeotawithlog'
-
-#alias lunch.gtv-4.0-aosp-jb-mr1='cd $GOOGLETVDIR/gtv-4.0-aosp-jb-mr1; . build/envsetup.sh; lunch cosmo-eng; source.devsetup'
-#alias build.gtv-4.0-aosp-jb-mr1='cd $GOOGLETVDIR/gtv-4.0-aosp-jb-mr1; . build/envsetup.sh; lunch cosmo-eng; source.devsetup; makewithlog; makeotawithlog; gtv_reinstall'
-#alias night.gtv-4.0-aosp-jb-mr1='cd $GOOGLETVDIR/gtv-4.0-aosp-jb-mr1; . build/envsetup.sh; lunch cosmo-eng; source.devsetup; repo sync -j20; makewithlog; makeotawithlog'
-
-alias deletelog='echo "delete logfiles below..."; ll make_*.log 2>/dev/null; rm ./make_*.log; echo "remained logfiles below..."; find . -name "make_*.log"'
-alias makewithlog='$MAKEWITHLOG_SH'
-alias makeotawithlog='$MAKEOTAWITHLOG_SH'
-alias gtv_reinstall='$GTV_REINSTALL_SH'
-alias do_all_at_once='$DO_ALL_AT_ONCE_SH'
-
-export kernel=${WORK_MOUNTED}/tool/adt-bundle-linux/sdk/system-images/android-17/armeabi-v7a/kernel-qemu
-alias emulator.avd='emulator -kernel $kernel -system $OUT/system.img -datadir $OUT/userdata.img -ramdisk $OUT/ramdisk.img'
-export system_images=/home/kihwanl/tool/adt-bundle-linux/sdk/system-images/android-16/armeabi-v7a
-export system_images_org=/home/kihwanl/tool/adt-bundle-linux/sdk/system-images/android-16.org/armeabi-v7a
-alias cp.emulator='cp -ar $OUT/*.img $system_images'
-alias rb.emulator='cp -ar $system_images_org/*.img $system_images'
-alias cp.userdata='cp -ar $system_images_org/userdata.img $OUT'
-
-alias makeepkwithlog='time make epk -j8 2>&1 | tee make_epk_build.log; ll $OUT/cosmo.epk | grep $OUT/cosmo.epk'
-alias copyepk='cp $OUT/cosmo.epk /var/lib/tftpboot/cosmo/cosmo.epk; sudo chmod -R 777 /var/lib/tftpboot/*; ls -alRh /var/lib/tftpboot'
-
-alias mmwithlog='time mm -j8 2>&1 | tee make_mm_build.log'
-# alias make GtvVideoViewTest -j8
-alias pushapk='$PUSHAPK_SH'
-alias pushjar='$PUSHJAR_SH'
-alias do_apk_at_once='mmwithlog && pushapk'
-alias do_jar_at_once='mmwithlog && pushjar'
-
-
-# for debug
-alias jdbattach='$JDBATTACH_SH'
-alias gdbattach='$GDBATTACH_SH'
-
-alias whatamidoingnowAll='
-gotogit0; whatamidoingnow;
-gotogit1; whatamidoingnow;
-gotogit2; whatamidoingnow;
-gotogitv2.1; whatamidoingnow;
-gotogitv3.0; whatamidoingnow;
-gotogitv4.0; whatamidoingnow;
-gotolg0; whatamidoingnow;
-gotolgv3.0; whatamidoingnow;
-'
-
-alias checkremote.help='echo "repo start checkremote ."; echo "git remote update"; echo "git branch -a"; echo "git diff checkremote remotes/m/master"; echo "repo abandon checkremote"'
-alias checkremote='repo start checkremote .; git remote update; git branch -a; git diff checkremote remotes/m/master; repo abandon checkremote'
-
-
 alias make.ctags='$ENVSETUP/makectags.sh'
 alias make.cscope='$ENVSETUP/makecscope.sh'
 alias make.filelist='rm filelist'
@@ -260,21 +116,6 @@ alias whichpkg='$WHICHPKG_SH'
 alias findtest='$FINDTEST_SH'
 alias listtest='runtest -l'
 
-alias mk='make clean; make; make calm_clean'
-
 alias chrome='google-chrome'
-
-alias test.launchHome='amstart -a android.intent.action.MAIN -c android.intent.category.HOME -n com.google.tv.launcher/.HomeActivity'
-alias test.onBoot='amstart -a android.intent.action.BOOT_COMPLETED -f 0x00000010'
-
-alias ecosenv='. /opt/ecos/ecosenv.sh'
-
-alias copy2glibc='croot; cp out/target/product/fox_glibc/system/lib/* out/target/product/fox/system/glibc; find ./out -name libOmxCore.so -exec ls -al {} \;'
-
-export T32SYS=/opt/t32
-export T32TMP=/tmp
-export T32ID=T32
-
-export ADOBE_PATH=/opt/Adobe/Reader9
 
 echo "    ~/.envsetup.$OS/devsetup.sh sourced!!!"
--- pathsetup.sh
+++ pathsetup.sh
@@ -4,65 +4,31 @@
 PATH=${PATH/\/usr\/local\/buildtools\/java\/jdk\/bin:/}
 
 PATH=$PATH:$envsetup
-PATH=$PATH:$envsetup:$envsetup/gtv
 PATH=/usr/local/bin:$PATH
-PATH=$PATH:/home/k/download/global/global
-PATH=$PATH:/home/k/download/global/gtags
-PATH=/works/studies/rtos/S5PC100/SRC/Linux/GCC/4.3.1-eabi-armv6/usr/bin:$PATH
-LD_LIBRARY_PATH=/works/studies/rtos/S5PC100/SRC/Linux/GCC/4.3.1-eabi-armv6/gmp/lib:/works/studies/rtos/S5PC100/SRC/Linux/GCC/4.3.1-eabi-armv6/mpfr/lib
+PATH=$PATH:$JAVA_HOME/bin
 export PATH LD_LIBRARY_PATH
-
-llvm_bin=/home/k/dev/llvm31_install/bin
-if [[ "$llvm_bin" != "" ]]; then
-    PATH=$llvm_bin:$PATH
-fi
-
-t32_bin=/opt/t32/bin/pc_linux64
-if [[ "$t32_bin" != "" ]]; then
-    PATH=$t32_bin:$PATH
-fi
 
 jdk_bin=~/tool/jdk1.6.0_45/bin
 if [[ "$jdk_bin" != "" ]]; then
     PATH=$jdk_bin:$PATH
 fi
 
-android_bin=~/tool/android/bin
-if [[ "$android_bin" != "" ]]; then
-    PATH=$android_bin:$PATH
-fi
-
-android_platform_tools=~/tool/android/adt-bundle/sdk/platform-tools
+android_platform_tools=~/tool/adt-bundle/sdk/platform-tools
 if [[ "$android_platform_tools" != "" ]]; then
     PATH=$android_platform_tools:$PATH
 fi
 
-android_tools=~/tool/android/adt-bundle/sdk/tools
+android_tools=~/tool/adt-bundle/sdk/tools
 if [[ "$android_tools" != "" ]]; then
     PATH=$android_tools:$PATH
 fi
 
-eclipse_bin=~/tool/android/adt-bundle/eclipse
+eclipse_bin=~/tool/adt-bundle/eclipse
 if [[ "$eclipse_bin" != "" ]]; then
     PATH=$PATH:$eclipse_bin
 fi
 
-argouml_path=~/tool/freeware/argouml-0.34
-if [[ "$argouml_path" != "" ]]; then
-    PATH=$PATH:$argouml_path
-fi
-
-ecos_path=/opt/ecos/ecos-3.0/tools/bin
-if [[ "$ecos_path" != "" ]]; then
-    PATH=$PATH:$ecos_path
-fi
-
-arm_eabi_path=~/tool/gcc/linux-x86/arm/arm-eabi-4.6/bin
-if [[ "$arm_eabi_path" != "" ]]; then
-    PATH=$PATH:$arm_eabi_path
-fi
-
-arm_2009q3_path=~/tool/gcc/arm-2009q3/bin
+arm_2009q3_path=~/tool/arm-2009q3/bin
 if [[ "$arm_2009q3_path" != "" ]]; then
     PATH=$PATH:$arm_2009q3_path
 fi
@@ -75,16 +41,6 @@ BLUE="\[\e[0;34m\]"
 END="\[\e[m\]"
 GIT_BRANCH="\`ruby -e \"print (%x{git branch 2> /dev/null}.split(%r{\n}).grep(/^\*/).first || '').gsub(/^\* (.+)$/, '(\1) ')\"\`"
 GIT_BR_TAG='$(__git_ps1)'
-#GIT_BR_TAG="\`ruby -e \"print (%x{git branch 2> /dev/null}.split(%r{\n}).grep(/^\*/).first || '').gsub(/^\* (.+)$/, '\1/')\" -e \"print (%x{git describe --tags 2> /dev/null}.split(%r{\n}).first || '').gsub(/\*/, '\1')\"\`"
-#PS1="${GRAY}\u@\h${END} ${GREEN}\w${END} $GIT_BRANCH${BLUE}$ ${END}"
 PS1="${GREEN}\w${END}${GIT_BR_TAG}${BLUE}$ ${END}"
-#PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-#PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-#PS1="${GRAY}\u@\h${END} ${GREEN}\w${END} $(__git_ps1 "(%s)")${BLUE}$ ${END}"
-
-# show (git branch) only
-#PS1="\[\e[0;32m\]\u@\h\[\e[m\] \[\e[1;32m\]\w\[\e[m\] \`ruby -e \"print (%x{git branch 2> /dev/null}.split(%r{\n}).grep(/^\*/).first || '').gsub(/^\* (.+)$/, '(\1) ')\"\`\[\e[0;32m\]\$ \[\e[m\]\[\e[0;32m\]"
-# show (git branch / git tag) both
-#PS1="\[\e[0;32m\]\u@\h\[\e[m\] \[\e[1;32m\]\w\[\e[m\] \`ruby -e \"print (%x{git branch 2> /dev/null}.split(%r{\n}).grep(/^\*/).first || '').gsub(/^\* (.+)$/, '\1/')\" -e \"print (%x{git describe --tags 2> /dev/null}.split(%r{\n}).first || '').gsub(/\*/, '\1')\"\`\[\e[0;32m\]\$ \[\e[m\]\[\e[0;32m\]"
 
 echo "    ~/.envsetup.$OS/pathsetup.sh sourced!!!"
